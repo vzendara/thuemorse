347f2c8bf271b168316a68a103fa6f056810e904
tffarrell-vz <farrell@abiresearch.com>
2014-08-30 14:32:01 -0400

    Removed unneccessary function from string approach to binary flipping.

--- thuemorse.py
+++ thuemorse.py
@@ -1,15 +1,3 @@
-def bitwise_negation(num):
-    negation = ""
-    for digit in num:
-        if digit == "0":
-            negation = negation + "1"
-        elif digit == "1":
-            negation = negation + "0"
-        else:
-            print "Error in the negation loop"
-    return negation
-
-
 def thue(n):
     if n == 0:
         return "0"
